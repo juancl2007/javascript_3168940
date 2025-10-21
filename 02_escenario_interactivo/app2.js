73c2f7ccea65044fa3c03ffce10428ab01747c3c
juancl2007 <lmjcamilo@gmail.com>
2025-10-21 17:26:21 -0500

    escenario Interactivo

---
+++ 02_escenario_interactivo/app2.js
@@ -0,0 +1,26 @@
+const personajes = document.querySelector('.personajes');
+const enemigos = document.querySelectorAll('.enemigos');
+const monedas = document.querySelectorAll('.monedas');
+const vidas = document.querySelector('.vidas');
+const contador = document.querySelector('.contador');
+
+let counter = 0;
+
+enemigos.forEach(item => {
+
+    item.addEventListener('click', () => {
+        item.style.filter = "grayscale(100%)";
+        item.classList.add("saltar");
+        vidas.textContent = parseInt(vidas.textContent) + 1;
+    });
+});
+
+
+monedas.forEach(m => {
+
+    m.addEventListener('click', () => {
+        m.style.filter = "grayscale(100%)";
+        m.classList.add("saltar");
+        contador.textContent = parseInt(contador.textContent) + 1;
+    });
+});
