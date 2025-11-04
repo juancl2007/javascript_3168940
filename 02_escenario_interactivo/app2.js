026fe704497f80bddc8b95450838a84b1bfe666e
juancl2007 <lmjcamilo@gmail.com>
2025-11-04 17:22:53 -0500

    escenario interactivo con sonidos

--- 02_escenario_interactivo/app2.js
+++ 02_escenario_interactivo/app2.js
@@ -3,6 +3,13 @@ const enemigos = document.querySelectorAll('.enemigos');
 const monedas = document.querySelectorAll('.monedas');
 const vidas = document.querySelector('.vidas');
 const contador = document.querySelector('.contador');
+const sonido= document.querySelector('.sonido');
+const zombieSound= document.querySelector('.zombie');
+const haloSound= document.querySelector('.halo');
+
+personajes.addEventListener('click', () => {
+    haloSound.play();
+});
 
 let counter = 0;
 
@@ -12,6 +19,7 @@ enemigos.forEach(item => {
     item.addEventListener('click', () => {
         item.style.filter = "grayscale(100%)";
         item.classList.add("saltar");
+        zombieSound.play();
         vidas.textContent = parseInt(vidas.textContent) + 1;
     });
 });
@@ -21,6 +29,7 @@ monedas.forEach(m => {
 
     m.addEventListener('click', () => {
         m.style.filter = "grayscale(100%)";
+        sonido.play();
         m.classList.add("saltar");
         contador.textContent = parseInt(contador.textContent) + 1;
     });
@@ -30,12 +39,21 @@ const balones = document.querySelectorAll('.balones');
 const manzanas = document.querySelectorAll('.manzanas');
 const contadorManzanas = document.querySelector('.contadorManzanas');
 const contadorBalones = document.querySelector('.contadorBalones');
+const sonidoBalon= document.querySelector('.sonidoBalon');
+const caidaSound= document.querySelector('.caida');
+const grada = document.querySelector('.grada');
+const multitudSound= document.querySelector('.multitud');
+
+grada.addEventListener('click', () => {
+    multitudSound.play();
+});
 
 balones.forEach(balon => {
     balon.addEventListener('click', () => {
         //balon.style.filter = "grayscale(100%)";
         balon.style.animation = "salto 3s ease forwards";
        //balon.classList.add("saltar");
+        sonidoBalon.play();
         contadorBalones.textContent = parseInt(contadorBalones.textContent) + 1;
     });
 });
@@ -44,6 +62,7 @@ manzanas.forEach(manzana => {
     manzana.addEventListener('click', () => {
         manzana.style.filter = "grayscale(100%)";
         manzana.classList.add("saltar");
+        caidaSound.play();
         contadorManzanas.textContent = parseInt(contadorManzanas.textContent) + 1;
     });
 });
@@ -52,11 +71,20 @@ const guardias = document.querySelectorAll('.guardias');
 const galletas = document.querySelectorAll('.galletas');
 const contadorSquid = document.querySelector('.contadorSquid');
 const contadorF = document.querySelector('.contadorF');
+const munecaSound= document.querySelector('.green');
+const muneca = document.querySelector('.muneca');
+const disparos = document.querySelector('.disparos');
+const comer = document.querySelector('.comer');
+
+muneca.addEventListener('click', () => {
+    munecaSound.play();
+});
 
 guardias.forEach(guardia => {
     guardia.addEventListener('click', () => {
         guardia.style.filter = "contrast(700%)";
         guardia.classList.add("saltar");
+        disparos.play();
         contadorSquid.textContent = parseInt(contadorSquid.textContent) + 1;
     });
 });
@@ -65,6 +93,7 @@ galletas.forEach(galleta => {
     galleta.addEventListener('click', () => {
         galleta.style.animation = "salto 2s ease forwards";
         galleta.classList.add("saltar");
+        comer.play();
         contadorF.textContent = parseInt(contadorF.textContent) + 1;
     });
 });
