f54007320ddf4ae827621c375f40df906fc77ef1
juancl2007 <lmjcamilo@gmail.com>
2025-11-06 17:12:48 -0500

    Base datos comic

---
+++ 03_Base_datos_Comic/bd.js
@@ -0,0 +1,41 @@
+let comic = {
+  "nombrecomic": "Un legado que resuena en el presente",
+  "numeroCapitulos": "3",
+  "genero": "historia",
+  "sipnosis": "Super tower",
+  "autores": "Juan lopez, juan polo, daniel hicapie",
+  "portadaComic": "imagenes/portada1.jpg",
+  "año": 2025 [
+    {
+      "id": 1,
+      "nombre": "El nacimiento de un Eco musical",
+      "personjes": ["rodolfo","abuela"],
+      "portada": "imagenes/capitulo1.jpg",
+      "descripcion": [ 
+      "el capitulo narra el nacimiento de rodolfo donde descubre su pasion por la musica y como su abuela lo inspira a seguir sus sueños." ;
+      ]
+    }, 
+    {
+      "id": 2,
+      "nombre": "El alma detras de la voz inmortal",
+      "personjes": ["rodolfo","bandas famosas"],
+      "portada": "imagenes/capitulo2.jpg",
+      "descripcion": [ 
+        "el capitulo explora la carrera musical de rodolfo, sus desafios y como su voz se convierte en un simbolo de epoca decembrina para muchos." ;
+        ]
+    },
+    {
+      "id": 3,
+      "nombre": "Un legado que trasciende generaciones",
+      "personjes": "rodolfo","mateo",
+      "portada": "imagenes/capitulo3.jpg",
+      "descripcion": [
+      "el capitulo final muestra el legado que dejo rodolfo cuando murio"
+      "mateo un joven de 17 años encuentra una casetera vieja en el atico de su abuelo y ahi
+       "descubre la musica de rodolfo inspirandose en su historia decide seguir sus pasos y llevar su legado adelante." ;
+     ] 
+    }
+ ]
+}
+
+
