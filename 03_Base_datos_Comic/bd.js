5cd68e651d36b671531db74988c988757702e639
juancl2007 <lmjcamilo@gmail.com>
2025-12-10 17:16:20 -0500

    Pagina Terminada Completamente

--- 03_Base_datos_Comic/bd.js
+++ 03_Base_datos_Comic/bd.js
@@ -6,13 +6,13 @@ export let comic = {
   "autores": "Juan lopez, juan polo, daniel hicapie",
   "carrusel": [
     {
-  "portadaComic1": "./fondo1.png",
-  "portadaComic2": "./fondo2.png",
-  "portadaComic3": "./fondo3.jpg",
+      "portadaComic1": "./fondo1.png",
+      "portadaComic2": "./fondo2.png",
+      "portadaComic3": "./fondo3.jpg"
     }  
-],
+  ],
   "año": 2025 ,
-   "personajes": [
+  "personajes": [
     {
       "id": 1,
       "NombrePersonaje": "Rodolfo Aicardi",
@@ -20,10 +20,10 @@ export let comic = {
       "descripcion2": "Marco Tulio Aicardi Rivera, conocido artísticamente como Rodolfo Aicardi, es el protagonista central de esta historia. Nacido en Galeras, Sucre, su vida se transforma al descubrir su pasión por la música desde una edad temprana. A los quince años, se muda a Medellín para perseguir su sueño musical, donde su voz rica y poderosa lo lleva a convertirse en un ícono de la cumbia y el porro. A lo largo del cómic, se exploran sus triunfos, desafíos y el impacto duradero de su música en la cultura colombiana, culminando en un legado que trasciende generaciones.",
       "imagen": "./rodolfo.png",
       "datos": {
-        "Biografía": "Biografia: Nació el 23 de mayo de 1946 y falleció el 24 de octubre de 2007.",
-        "Discografía": "Discografia: Incluye éxitos como 'La Colegiala', 'Cariñito' y 'Tabaco y Ron'.",
-        "Legado": "Legado: Considerado un ícono de la música tropical en Colombia, su música sigue siendo popular en fiestas y celebraciones.",
-      },
+        "Biografía": "Biografía: Nació el 23 de mayo de 1946 y falleció el 24 de octubre de 2007.",
+        "Discografía": "Discografía: Incluye éxitos como 'La Colegiala', 'Cariñito' y 'Tabaco y Ron'.",
+        "Legado": "Legado: Considerado un ícono de la música tropical en Colombia, su música sigue siendo popular en fiestas y celebraciones."
+      }
     },
     {
       "id": 2,
@@ -32,9 +32,9 @@ export let comic = {
       "descripcion2": "La abuela de Rodolfo desempeña un papel fundamental en su infancia, siendo una fuente constante de apoyo y motivación. Su amor por la música y su aliento inquebrantable ayudan a moldear el camino de Rodolfo hacia su destino musical. A través de su influencia, se establece una conexión emocional profunda que impulsa al joven a perseguir sus sueños con determinación y pasión.",
       "imagen": "./abuelaa.png",
       "datos": {
-        "Biografía": "Biografia: Apoyó y motivó a Rodolfo en sus primeros años, inculcándole el amor por la música.",
-        "Influencia": "Influencia: Su apoyo fue crucial para que Rodolfo persiguiera su sueño musical.",
-      },
+        "Biografía": "Biografía: Apoyó y motivó a Rodolfo en sus primeros años, inculcándole el amor por la música.",
+        "Influencia": "Influencia: Su apoyo fue crucial para que Rodolfo persiguiera su sueño musical."
+      }
     },
     {
       "id": 3,
@@ -43,12 +43,11 @@ export let comic = {
       "descripcion2": "Mateo es un adolescente contemporáneo que representa la nueva generación que redescubre y honra el legado de Rodolfo Aicardi. Al encontrar un viejo casete de Rodolfo, Mateo se siente inspirado a aprender y tocar sus canciones, fusionando los ritmos clásicos de la cumbia con sonidos modernos. Su viaje musical no solo conecta el pasado con el presente, sino que también simboliza la continuidad del impacto cultural de Rodolfo en las nuevas generaciones.",
       "imagen": "./mateoo.png",
       "datos": {
-        "Biografía": "Biografia: Joven apasionado por la música que encuentra inspiración en el legado de Rodolfo.",
-        "Legado": "Legado: Representa la continuidad del impacto de Rodolfo en nuevas generaciones.",
-      },
-    },
+        "Biografía": "Biografía: Joven apasionado por la música que encuentra inspiración en el legado de Rodolfo.",
+        "Legado": "Legado: Representa la continuidad del impacto de Rodolfo en nuevas generaciones."
+      }
+    }
   ],
-
   "capitulos": [
     {
       "id": 1,
@@ -56,7 +55,7 @@ export let comic = {
       "personje": ["rodolfo","abuela"],
       "portada": "./cap1.png",
       "descripcion": [ 
-      "El joven Marco Tulio (Rodolfo Aicardi) descubre su pasión por los sonidos en su pueblo natal, Galeras. A los 15 años se muda a Medellín para perseguir su sueño, donde se convierte en un ícono de la cumbia." ,
+        "El joven Marco Tulio (Rodolfo Aicardi) descubre su pasión por los sonidos en su pueblo natal, Galeras. A los 15 años se muda a Medellín para perseguir su sueño, donde se convierte en un ícono de la cumbia." 
       ],
       "descripcion2": [
         "La vida de Marco Tulio Aicardi Rivera, el futuro Rodolfo Aicardi, se tejió entre la quietud ancestral de Galeras, Sucre, y el vibrante dinamismo de Medellín. Impulsado por una profunda sensibilidad hacia el mundo sonoro de su niñez, Aicardi transformó su destino al dejar su tierra natal a los quince años. En la capital antioqueña, su voz rica y poderosa se consolidó, primero como integrante de agrupaciones fundamentales como el Sexteto Miramar y Los Hispanos, y posteriormente como director de su propia orquesta, La Típica RA7. Este recorrido lo estableció como un ícono de la música tropical, convirtiendo su interpretación de cumbias, porros y boleros en la banda sonora esencial e inmortal de las fiestas decembrinas colombianas."
@@ -64,12 +63,12 @@ export let comic = {
     }, 
     {
       "id": 2,
-      "nombre": "CAPITULO 2: El alma detras de la voz inmortal",
+      "nombre": "CAPITULO 2: El alma detrás de la voz inmortal",
       "personje": ["rodolfo","bandas famosas"],
       "portada": "./cap2.png",
       "descripcion": [ 
-      "Rodolfo Aicardi se vuelve famoso y su voz se convierte en la banda sonora de las fiestas colombianas. Aunque disfrutaba del éxito, su música también era su refugio personal. A pesar de su muerte en 2007, su voz sigue viva." ,
-        ],
+        "Rodolfo Aicardi se vuelve famoso y su voz se convierte en la banda sonora de las fiestas colombianas. Aunque disfrutaba del éxito, su música también era su refugio personal. A pesar de su muerte en 2007, su voz sigue viva." 
+      ],
       "descripcion2": [
         "A pesar de la fama y los éxitos internacionales, Rodolfo Aicardi mantuvo una conexión inquebrantable con la esencia cultural colombiana, erigiéndose como el guardián de la cumbia y el porro en un panorama musical cambiante. Más allá de su habilidad para encender la pista de baile, Aicardi fue un letrista sensible, cuyas composiciones eran verdaderos poemas que narraban la nostalgia, el amor y la rica tapicería cultural de su nación. Su voz, que se convirtió en la banda sonora de la identidad colombiana y un puente generacional, lo transformó en un símbolo del pueblo. Aunque su partida en 2007 dejó un vacío inmenso, el legado de Aicardi perdura, confirmando la promesa de que ciertas melodías poseen una fuerza y autenticidad tales que alcanzan la inmortalidad."
       ]
@@ -80,14 +79,12 @@ export let comic = {
       "personje": ["rodolfo","mateo"] ,
       "portada": "./cap3.png",
       "descripcion": [ 
-      "Un adolescente, Mateo, encuentra un casete de Rodolfo Aicardi. Fascinado, aprende a tocar sus canciones, fusionando lo clásico con lo moderno. Al final, Mateo canta en una plaza, manteniendo vivo el legado del artista." ,
-     ],
-     "descripcion2": [
+        "Un adolescente, Mateo, encuentra un casete de Rodolfo Aicardi. Fascinado, aprende a tocar sus canciones, fusionando lo clásico con lo moderno. Al final, Mateo canta en una plaza, manteniendo vivo el legado del artista." 
+      ],
+      "descripcion2": [
         "El legado de Rodolfo Aicardi se manifiesta en el presente a través de nuevas generaciones, ejemplificado en la historia de Mateo, un adolescente de la Medellín moderna que, al descubrir un viejo casete, queda cautivado por la emoción y la fuerza de su voz. Su investigación sobre Aicardi se convierte en un viaje de autodescubrimiento, conectándolo con la historia de su familia, su país y sus profundas raíces culturales. Al fusionar los ritmos clásicos de la cumbia con sonidos contemporáneos, Mateo honra la obra del maestro, culminando en un emotivo momento en una plaza pública: jóvenes y ancianos se unen para cantar la melodía de Aicardi. Este acto demuestra que la música de Rodolfo Aicardi no ha muerto, sino que se ha transformado en un eco eterno, resonando en el corazón de quienes la escuchan y uniendo pasado y presente en una melodía que se niega a desaparecer."
       ]
     }
- ],
-
+  ]
 }
-
 
